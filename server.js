c46a25fcd8953e365587cc06a86082f74125a109
Bruno Monteiro <brunomonteiro@Brunos-MBP.home>
2019-07-09 18:02:57 +0100

    working

--- server.js
+++ server.js
@@ -26,25 +26,31 @@ app.get('/chat', function(req, res) {
     res.sendFile((__dirname + '/chat.html'));
 });
 
-app.get('/chat3', function(req, res) {
-    res.sendFile((__dirname + '/chat3.html'));
+app.get('/privateDiana', function(req, res) {
+    res.sendFile((__dirname + '/privateDiana.html'));
 });
 
 
 io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
+    io.sockets.emit('get users', users);
+
+    
 
     // Disconnect
     socket.on('disconnect', function(data){
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
+        if(socket.username){
+            console.log('warning')
+            users.splice(users.indexOf(socket.username), 1);
+            updateUsernames();
+        }
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s socket connected', connections.length)
+
     });
     // Send message
     socket.on('send message', function(data){
-        console.log(data);
         io.sockets.emit('new message', {msg: data, user: socket.username});
     });
 
